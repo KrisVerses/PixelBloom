67177c2f993ea2a1879110c7e3744bd970dfcac1
KrisVerses <krisincode@gmail.com>
2025-03-12 10:42:23 -0500

    chore: wrap app in State Provider && write comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./app/routes.tsx";
 import "./index.css";
+import StateProvider from "./state/StateProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  //provides the state to the entire app
+  <StateProvider>
     <RouterProvider router={router} />
-  </StrictMode>
+  </StateProvider>
 );
--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,11 @@
-/// <reference types="vite/client" />
-//this file is to declare the types of the environment variables so that we can use them in our code
+// Define an interface for environment variables specific to Vite
 interface ImportMetaEnv {
+  // Declare a read-only property for the Unsplash access key
   readonly VITE_UNSPLASH_ACCESS_KEY: string;
 }
 
+// Define an interface for the import meta object
 interface ImportMeta {
+  // Declare a read-only property for the environment variables
   readonly env: ImportMetaEnv;
 }
